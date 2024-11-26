f695cca347aea786a47c0819d8652b99461438c6
robiul.islam8726 <robiul.islam8726@gmail.com>
2024-11-26 14:46:56 +0900

    added map

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,8 +1,8 @@
 const Board = () => {
     return (
         <div className="flex mx-auto max-w-[1000px] h-[260px]">
-            <div className="bg-green-600 pt-9 pr-14 pb-9 pl-14 w-[450px]"><h2 className="">We just moved to TORANOMON 4th March 2014 </h2></div>
-            <div className="pt-9 pr-10 pb-9 pl-10 w-[550px]"><p>このたび弊社は、東京オフィスを東京都港区虎ノ門に移転し ２０１４年３月４日(火)大安より営業の運びとなりました。 新オフィスは、打ち合わせスペースも多くありますので お近くにお越しの際はどうぞお気軽にお立ち寄りください。 今後とも弊社をどうぞよろしくお願い申し上げます。 </p></div>
+            <div className="bg-green-600 pt-9 pr-14 pb-9 pl-14 w-[450px]"><h2 className="text-5xl">We just moved to TORANOMON 4th March 2014 </h2></div>
+            <div className="pt-9 pr-10 pb-9 pl-10 w-[550px]"><p className="text-center text-sm md:text-center">このたび弊社は、東京オフィスを東京都港区虎ノ門に移転し ２０１４年３月４日(火)大安より営業の運びとなりました。 新オフィスは、打ち合わせスペースも多くありますので お近くにお越しの際はどうぞお気軽にお立ち寄りください。 今後とも弊社をどうぞよろしくお願い申し上げます。 </p></div>
             
             
         </div>
