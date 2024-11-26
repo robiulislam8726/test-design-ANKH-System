48726c9f4374553979fdd7d3df71b71f2db44d4c
robiul.islam8726 <robiul.islam8726@gmail.com>
2024-11-26 13:03:03 +0900

    added react icon

--- src/components/Card/Card4.jsx
+++ src/components/Card/Card4.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
+import { FaMobileAlt } from "react-icons/fa";
+
 
 const Card4 = ({ title, description }) =>  (
     <div className="bg-red-50 shadow-lg p-4 rounded-lg overflow-hidden">
       <div className="p-4">
-        <h2 className="font-bold text-lg">{title}</h2>
+      <h2 className="w-[300px] font-bold text-lg"> <FaMobileAlt /> {title}</h2>
         <p className="text-gray-700">{description}</p>
       </div>
     </div>
