e503c8c6bad0399beccde5fed5526bc541d26357
robiul.islam8726 <robiul.islam8726@gmail.com>
2024-11-28 13:32:21 +0900

    added board design with responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,26 +1,26 @@
 
 import banner from '../../assets/banner.png';
 import toranamon from '../../assets/TORANOMON.png';
-import Board from '../Board/Board';
+
 
 
 
 const Banner = () => {
-    return (
-      <div className="relative w-full">
-        {/* Banner Image */}
-        <img src={banner} className="w-full h-full object-cover"/>
-        {/* Title */}
-        <div className="top-6 left-0 absolute flex flex-col justify-center items-center bg-black bg-opacity-50 w-full md:h-[150px]">
-          {/* <h1 className="font-bold text-6xl text-white"><span className="font-thin">Hello</span> Toramon</h1> */}
-          <img src={toranamon} alt="" />
-          <h4 className="mt-2 text-white">あいうえお かきくけこ さしすせそ たちつてと なにぬねの はひふへほ まみむめも やゆよ らりるれろ わをん</h4>
-        </div>
-        
+  return (
+    <div className="relative w-full">
+      {/* Banner Image */}
+      <img src={banner} className="w-full h-full object-cover" />
+      {/* Title */}
+      <div className="top-6 left-0 absolute flex flex-col justify-center items-center bg-black bg-opacity-50 w-full md:h-[150px]">
+        {/* <h1 className="font-bold text-6xl text-white"><span className="font-thin">Hello</span> Toramon</h1> */}
+        <img className='w-fit' src={toranamon} alt="" />
+        <h4 className="mt-2 text-sm text-white">あいうえお かきくけこ さしすせそ たちつてと なにぬねの はひふへほ まみむめも やゆよ らりるれろ わをん</h4>
       </div>
-     
-      
-    );
-  };
-  
-  export default Banner;
+
+    </div>
+
+
+  );
+};
+
+export default Banner;
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,10 +1,22 @@
 const Board = () => {
     return (
-        <div className="flex mx-auto max-w-[1000px] h-[260px]">
-            <div className="bg-green-600 pt-9 pr-14 pb-9 pl-14 w-[450px]"><h2 className="text-5xl">We just moved to TORANOMON 4th March 2014 </h2></div>
-            <div className="pt-9 pr-10 pb-9 pl-10 w-[550px]"><p className="text-center text-sm md:text-center">このたび弊社は、東京オフィスを東京都港区虎ノ門に移転し ２０１４年３月４日(火)大安より営業の運びとなりました。 新オフィスは、打ち合わせスペースも多くありますので お近くにお越しの際はどうぞお気軽にお立ち寄りください。 今後とも弊社をどうぞよろしくお願い申し上げます。 </p></div>
-            
-            
+        <div className="flex md:flex-row flex-col mx-auto mt-2 max-w-[1000px] h-auto md:h-[260px]">
+            {/* Left Section*/}
+            <div className="bg-green-600 p-4 md:pt-9 md:pr-14 md:pb-9 md:pl-14 w-full md:w-[450px]">
+                <h2 className="text-justify text-white">  
+                    <span className="block font-thin text-4xl">We just moved to</span>
+                    <span className="block font-bold text-4xl">TORANOMON</span>
+                    <span className="block font-thin text-5xl">4th March 2014 </span>
+                </h2>
+            </div>
+
+
+            {/* Right Section*/}
+
+            <div className="pt-9 pr-10 pb-9 pl-10 w-[550px]">
+                <p className="text-center text-sm md:text-center">このたび弊社は、東京オフィスを東京都港区虎ノ門に移転し ２０１４年３月４日(火)大安より営業の運びとなりました。 新オフィスは、打ち合わせスペースも多くありますので お近くにお越しの際はどうぞお気軽にお立ち寄りください。 今後とも弊社をどうぞよろしくお願い申し上げます。 </p>
+
+            </div>
         </div>
     );
 };
